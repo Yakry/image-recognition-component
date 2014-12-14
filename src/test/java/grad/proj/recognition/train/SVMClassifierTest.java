904a485e3f50ccebd2a762d5175f250bad600320
Mahmoud Mohsen <mahmoud.mohsen.213@outlook.com>
2014-12-14 23:16:10 +0200

    SVMClassifierTest class update SVMClassifierTest.testRuntime initial implementation test cases input files

--- src/test/java/grad/proj/recognition/train/SVMClassifierTest.java
+++ src/test/java/grad/proj/recognition/train/SVMClassifierTest.java
@@ -1,12 +1,56 @@
 package grad.proj.recognition.train;
 
 import static org.junit.Assert.*;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 import org.junit.Test;
 
 public class SVMClassifierTest {
 	
 	@Test
-	public void testSVMClassifier() throws Exception {
+	public void testRunTime() throws Exception {
+		Scanner scanner = new Scanner(new FileInputStream("splice_scale.txt"));
+		List<List<Double> > featureVectors = new ArrayList<List<Double>>();
+		List<Double> featureVector;
+		List<Double> rightFeatureVector = null;
+		List<Double> wrongFeatureVector = null;
 		
+		int classNum = scanner.nextInt();
+		int vectorNum = scanner.nextInt();
+		int featureNum = scanner.nextInt();
+		
+		while(scanner.hasNextInt()){
+			int classLabel = scanner.nextInt();
+			int idx=1;
+			featureVector = new ArrayList<Double>();
+			while(!scanner.hasNextInt()&&scanner.hasNext()&&(idx<featureNum)){
+				String temp = scanner.next();
+				int colonIdx = temp.indexOf(':');
+				int tempIdx = Integer.valueOf(temp.substring(0,colonIdx));
+				double val = Double.valueOf(temp.substring(colonIdx+1));
+				for(;idx<tempIdx;++idx)
+					featureVector.add(0.0);
+				featureVector.add(val);
+				++idx;
+			}
+			
+			featureVectors.add(featureVector);
+		}
+		
+		rightFeatureVector = featureVectors.get(0);
+		wrongFeatureVector = new ArrayList<Double>();
+		for(int i=0;i<featureNum;++i)
+			wrongFeatureVector.add(new Random().nextDouble());
+		
+		SVMClassifier classifier = new SVMClassifier();
+		classifier.train(featureVectors);
+		//System.out.println(classifier.classify(rightFeatureVector));
+		//System.out.println(classifier.classify(wrongFeatureVector));
+		scanner.close();
 	}
 }
