912f9957ff3c6619a74837dfc5f1fe2867821f50
Mahmoud Mohsen <mahmoud.mohsen.213@outlook.com>
2015-02-28 22:24:53 +0200

    added LinearNormalizer dummy testing

---
+++ src/test/java/grad/proj/recognition/train/LinearNormalizerTest.java
@@ -0,0 +1,15 @@
+package grad.proj.recognition.train;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.opencv.core.Core;
+
+public class LinearNormalizerTest {
+	static{System.loadLibrary(Core.NATIVE_LIBRARY_NAME);}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
