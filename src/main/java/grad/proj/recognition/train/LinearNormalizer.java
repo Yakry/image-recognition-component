f2cc8961ce239936bcd5a2426f63fe8e9ed66d1a
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2015-03-11 07:09:28 +0200

    fixed compilation error, removed a test that just fails without doing nothing

--- src/main/java/grad/proj/recognition/train/LinearNormalizer.java
+++ src/main/java/grad/proj/recognition/train/LinearNormalizer.java
@@ -6,83 +6,6 @@ import java.io.*;
 import java.util.Collections;
 
 public class LinearNormalizer implements Normalizer {
-<<<<<<< HEAD
-	public List<Double> Max ;
-    public List<Double> Min ;
-
-@Override
-public List<List<Double>> reset(List<List<Double>> featureVectors) {
-
-	  double largest   = 0.0;
-      double smallest  = 0.0;
-      double newnumber = 0.0;
-      
-   
-     Max=new ArrayList<Double>(featureVectors.size());//array of maximum elements in each column in the matrix
-     Min=new ArrayList<Double>(featureVectors.size());//array of minimum elements in each column in the matrix
-     
-     
-     for (int col=0; col < featureVectors.size(); col++) //get the max of each column
-           {       
-              largest = Integer.MIN_VALUE;
-                                   
-          for (int row=0; row < featureVectors.size(); row++)   
-               {           
-                    
-                   if (col >= featureVectors.get(row).size())
-                      {
-                          continue;
-                      }
-                                                           
-                  newnumber = featureVectors.get(row).get(col);
-                                                 
-                 if (newnumber > largest)
-                      {
-                         largest = newnumber;
-                      }
-                                      
-               }
-       
-               
-             Max.add(largest);
-              
-           }
-   /////////////////////////////////////////////////////////////////////
-     for (int col=0; col < featureVectors.size(); col++) //get the min of each column
-       {       
-          smallest = Integer.MAX_VALUE;
-                             
-    for (int row=0; row < featureVectors.size(); row++)   
-          {           
-                
-               if (col >= featureVectors.get(row).size())
-                  {
-                      continue;
-                  }
-                                                       
-              newnumber = featureVectors.get(row).get(col);
-                                           
-           if (newnumber < smallest)
-                  {
-                     smallest = newnumber;
-                  }
-                                  
-         }
-   
-           
-         Min.add(smallest);  
-       }
-
-     for (int i=0;i<featureVectors.size();i++)
-			for (int j=0;j<featureVectors.get(i).size();j++)
-		{
-				
-			featureVectors.get(i).set(j, (featureVectors.get(i).get(j)-Min.get(j))/(Max.get(j)-Min.get(j)));
-		}
-
-          return featureVectors;
-}
-=======
 	public List<Double> Max;
 	public List<Double> Min;
 
@@ -117,7 +40,6 @@ public List<List<Double>> reset(List<List<Double>> featureVectors) {
 		for (int col = 0; col < counter; col++) // get the max of each column
 		{
 			largest = Integer.MIN_VALUE;
->>>>>>> 1aab5eea7ef1b092547ed56458263fe4f12e5c09
 
 			for (int row = 0; row < featureVectors.size(); row++) {
 
@@ -132,11 +54,6 @@ public List<List<Double>> reset(List<List<Double>> featureVectors) {
 				}
 
 			}
-<<<<<<< HEAD
-    return featureVector;
-}
-=======
-
 			Max.add(largest);
 
 		}
@@ -187,6 +104,5 @@ public List<List<Double>> reset(List<List<Double>> featureVectors) {
 		}
 		return featureVector;
 	}
->>>>>>> 1aab5eea7ef1b092547ed56458263fe4f12e5c09
 
 }
