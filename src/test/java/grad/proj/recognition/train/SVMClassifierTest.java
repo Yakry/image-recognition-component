fff98e326b5b803f68d1ca9109eeab52294e121b
Mahmoud Mohsen <mahmoud.mohsen.213@outlook.com>
2015-03-12 00:22:29 +0200

    SVMClassifierTest refactored to use assertions and new multi class data testing

--- src/test/java/grad/proj/recognition/train/SVMClassifierTest.java
+++ src/test/java/grad/proj/recognition/train/SVMClassifierTest.java
@@ -1,6 +1,7 @@
 package grad.proj.recognition.train;
 
 import static org.junit.Assert.*;
+
 import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,25 +18,32 @@ public class SVMClassifierTest {
 	
 	@Test
 	public void testRunTime() throws Exception {
-		List<List<Double> > featureVectors = 
-				LinearNormalizerTest.
-				loadFeatureVectors("splice_scale.txt").get(0);
-		List<Double> rightFeatureVector = null;
-		List<Double> wrongFeatureVector = null;
+		List<List<List<Double> > > allClassData = LinearNormalizerTest.
+				loadFeatureVectors("src\\test\\java\\grad\\proj\\recognition\\train\\splice_scale.txt");
 		
-		rightFeatureVector = featureVectors.get(0);
-		wrongFeatureVector = new ArrayList<Double>();
-		for(int i=0;i<featureVectors.get(0).size();++i)
-			wrongFeatureVector.add(new Random().nextDouble());
+		for(int classLabel = 0;classLabel<allClassData.size();++classLabel){
+			List<List<Double> > featureVectors = allClassData.get(classLabel);
+			List<Double> rightFeatureVector = null;
+			List<Double> wrongFeatureVector = null;
+			
+			rightFeatureVector = featureVectors.get(0);
+			wrongFeatureVector = new ArrayList<Double>();
+			for(int i=0;i<featureVectors.get(0).size();++i)
+				wrongFeatureVector.add(new Random().nextDouble());
+			
+			SVMClassifier classifier = new SVMClassifier();
+			classifier.train(featureVectors);
+			double rightFeatureVectorValue = classifier.classify(rightFeatureVector);
+			double wrongFeatureVectorValue = classifier.classify(wrongFeatureVector);
+			System.out.println(rightFeatureVectorValue);
+			System.out.println(wrongFeatureVectorValue);
+			assertTrue("Right feature vector is not recongized at class " + classLabel,
+					rightFeatureVectorValue > 0.0);
+			assertTrue("Wrong feature vector is recongized at class " + classLabel,
+					wrongFeatureVectorValue == 0.0);
+		}
 		
-		SVMClassifier classifier = new SVMClassifier();
-		classifier.train(featureVectors);
-		double rightFeatureVectorValue = classifier.classify(rightFeatureVector);
-		double wrongFeatureVectorValue = classifier.classify(wrongFeatureVector);
-		System.out.println(rightFeatureVectorValue);
-		System.out.println(wrongFeatureVectorValue);
-		assertTrue("Right feature vector is not recongized", rightFeatureVectorValue > 0.0);
-		assertTrue("Wrong feature vector is recongized", wrongFeatureVectorValue == 0.0);
+		
 	}
 
 	@Test
