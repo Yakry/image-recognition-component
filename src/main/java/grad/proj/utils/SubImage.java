909d6116ee526185c099eead40d02ce25e5d4acf
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2015-06-20 10:54:13 +0200

    rewritten SubImageTests, refactor and fix bugs in SubImage class

--- src/main/java/grad/proj/utils/SubImage.java
+++ src/main/java/grad/proj/utils/SubImage.java
@@ -1,39 +1,47 @@
 package grad.proj.utils;
 
-
 public class SubImage implements Image {
 	
 	private Image originalImage;
 	
-	private int x;
-	private int y;
+	private int row;
+	private int col;
 	private int width;
 	private int height;
 	
-	public SubImage(Image originalImage, int x, int y, int width, int height) {
+	public SubImage(Image originalImage, int col, int row, int width, int height) {
 		this.originalImage = originalImage;
-		this.x = x;
-		this.y = y;
+		this.col = col;
+		this.row = row;
 		this.width = width;
 		this.height = height;
+		
+		if( row < 0 ||
+		    col < 0 || 
+		   (row + height) > originalImage.getHeight() ||
+		   (col + width) > originalImage.getWidth())
+				throw new RuntimeException("the sub image bounds are out of the original image bounds");
 	}
 
 	@Override
 	public int getPixelAt(int row, int col) {
-		if(row >= height  || col >= width ){
-			throw new RuntimeException("Access out of sub image bounds");
-		}
+		checkBounds(row, col);
 		
-		return originalImage.getPixelAt(x + row, y + col);
+		return originalImage.getPixelAt(this.row + row, this.col + col);
 	}
 
 	@Override
 	public void setPixelAt(int row, int col, int rgb) {
-		if(row >= height  || col >= width ){
-			throw new RuntimeException("Access out of sub image bounds");
-		}
+		checkBounds(row, col);
 
-		originalImage.setPixelAt(x + row, y + col, rgb);
+		originalImage.setPixelAt(this.row + row, this.col + col, rgb);
+	}
+
+	
+	private void checkBounds(int row, int col) {
+		if(row < 0 || col < 0 || row >= height  || col >= width ){
+			throw new IndexOutOfBoundsException("Access out of sub image bounds");
+		}
 	}
 
 	@Override
--- src/test/java/grad/proj/utils/SubImageTest.java
+++ src/test/java/grad/proj/utils/SubImageTest.java
@@ -1,27 +1,57 @@
 package grad.proj.utils;
 
-import java.awt.Color;
-import java.io.File;
-import java.nio.file.Paths;
+import java.util.Random;
 
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SubImageTest {
-	 
-	private static final String IMAGE_LOADING_TEST_PNG = "ImageLoadingTest.png";
-
+	
 	@Test
-	public void testLoadingImage() throws Exception{
-		assertNotNull(getClass().getResource(IMAGE_LOADING_TEST_PNG));
-		
-		File imageFile = new File(getClass().getResource(IMAGE_LOADING_TEST_PNG).toURI());
-		
-		Image originalImage = ImageLoader.loadImage(imageFile);
-		Image subImage = new SubImage(originalImage, 1, 0, 1, 2);
+	public void testGetPixelWithCorrectBounds() throws Exception{
 
-		assertEquals(originalImage.getPixelAt(1, 0), subImage.getPixelAt(0, 0));
-		assertEquals(originalImage.getPixelAt(1, 1), subImage.getPixelAt(0, 1));
+		Image originalImage = createTestImage();
+		
+		int startRow = 3, startCol = 5, width = 4, height = 4;
+		
+		Image subImage = new SubImage(originalImage, startCol, startRow, width, height);
+		
+		for(int row=0; row<subImage.getHeight(); row++){
+			for(int col=0; col<subImage.getWidth(); col++){
+				assertEquals(originalImage.getPixelAt(row + startRow, col + startCol), subImage.getPixelAt(row, col));
+			}
+		}
+	}
+	
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void testGetPixelWithOutOfBounds() throws Throwable{
+		Image originalImage = createTestImage();
+		
+		int startRow = 3, startCol = 5, width = 4, height = 4;
+		
+		Image subImage = new SubImage(originalImage, startCol, startRow, width, height);
+		subImage.getPixelAt(5, 3);
+	}
+
+	@SuppressWarnings("unused")
+	@Test(expected=RuntimeException.class)
+	public void testConstructSubImageOutOfBoundsOfOriginalImage() throws Throwable{
+		Image originalImage = createTestImage();
+		
+		int startRow = 3, startCol = 5, width = 12, height = 4;
+		
+		Image subImage = new SubImage(originalImage, startCol, startRow, width, height);
+	}
+
+	private Image createTestImage() {
+		Random random = new Random();
+		Image originalImage = new ImageImpl(10, 10);
+		for(int row=0; row<originalImage.getHeight(); row++){
+			for(int col=0; col<originalImage.getWidth(); col++){
+				originalImage.setPixelAt(row, col, random.nextInt());
+			}
+		}
+		return originalImage;
 	}
 }
