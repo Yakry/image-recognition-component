6df61eb85b870e4b999c948bea8482eabe22d09a
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2015-06-21 12:51:11 +0200

    SurfFeatureVectorGeneratorTests: creating random images instead of loading from file, removed subimage test

--- src/test/java/grad/proj/recognition/train/impl/SurfFeatureVectorGeneratorTest.java
+++ src/test/java/grad/proj/recognition/train/impl/SurfFeatureVectorGeneratorTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import grad.proj.recognition.RequiresLoadingTestBaseClass;
+import grad.proj.utils.ImageImpl;
+import grad.proj.utils.SubImageTest;
 import grad.proj.utils.TestsDataSetsHelper;
 import grad.proj.utils.FilesImageList;
 import grad.proj.utils.Image;
@@ -12,90 +14,49 @@ import grad.proj.utils.SubImage;
 import grad.proj.utils.TestsDataSetsHelper.DataSet;
 import grad.proj.utils.TestsDataSetsHelper.Type;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileWriter;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import org.junit.Test;
 import org.opencv.core.Mat;
 
 public class SurfFeatureVectorGeneratorTest extends RequiresLoadingTestBaseClass {
 
-	private static final String IMG1_BIG = "SURF_IMG_1_BIG.jpg";
-	private static final String IMG1_SMALL = "SURF_IMG_1_SMALL.jpg";
-
 	@Test
 	public void testFeatureVectorForSameImageDoesnotChange() throws Exception {
-		final URL IMG1_BIG_URL = getClass().getResource(IMG1_BIG);
-		final URL IMG1_SMALL_URL = getClass().getResource(IMG1_SMALL);
 
-		assertNotNull(IMG1_BIG_URL);
-		assertNotNull(IMG1_SMALL_URL);
-
+		Image image = SubImageTest.createRandomTestImage(100, 100);
+		
 		SurfFeatureVectorGenerator generator = new SurfFeatureVectorGenerator();
+		generator.prepareGenerator(Arrays.asList(Arrays.asList(image)));
 
-		Image img1Big = ImageLoader.loadImage(new File(IMG1_BIG_URL.toURI()));
-		//Image img1Small = ImageLoader.loadImage(new File(IMG1_SMALL_URL.toURI()));
-		
-		generator.prepareGenerator(Arrays.asList(Arrays.asList(img1Big)));
-
-		List<Double> generatedTry1 = generator.generateFeatureVector(img1Big);
-		
-		List<Double> generatedTry2 = generator.generateFeatureVector(img1Big);
+		List<Double> generatedTry1 = generator.generateFeatureVector(image);
+		List<Double> generatedTry2 = generator.generateFeatureVector(image);
 		
 		for(int i=0; i<generatedTry1.size(); i++)
 			assertEquals(generatedTry1.get(i), generatedTry2.get(i), 0.01);
-		
-		//assertArrayEquals(generatedTry1, generatedTry2, 0.0001f);
-
-//		System.out.println(generatedTry1.length + " " + Arrays.toString(generatedTry1));
-//		System.out.println(generatedTry2.length + " " + Arrays.toString(generatedTry2));
 	}
-
+	
 
 	@Test
 	public void testFeatureVectorForDifferentImagesShouldBeDifferent() throws Exception {
-		final URL IMG1_BIG_URL = getClass().getResource(IMG1_BIG);
-		final URL IMG1_SMALL_URL = getClass().getResource(IMG1_SMALL);
-
-		assertNotNull(IMG1_BIG_URL);
-		assertNotNull(IMG1_SMALL_URL);
-
+		Image image1 = SubImageTest.createRandomTestImage(100, 100);
+		Image image2 = SubImageTest.createRandomTestImage(100, 100);
+		
 		SurfFeatureVectorGenerator generator = new SurfFeatureVectorGenerator();
+		generator.prepareGenerator(Arrays.asList(Arrays.asList(image1, image2)));
 
-		Image img1Big = ImageLoader.loadImage(new File(IMG1_BIG_URL.toURI()));
-		Image img1Small = ImageLoader.loadImage(new File(IMG1_SMALL_URL.toURI()));
+		List<Double> generated = generator.generateFeatureVector(image1);
 		
-		generator.prepareGenerator(Arrays.asList(Arrays.asList(img1Big, img1Small)));
-
-		List<Double> generated = generator.generateFeatureVector(img1Big);
+		List<Double> generatedFromDifferentImage = generator.generateFeatureVector(image2);
 		
-		List<Double> generatedFromDifferentImage = generator.generateFeatureVector(img1Small);
-		
-		//System.out.println(generatedFromDifferentImage.cols() + " " + generatedFromDifferentImage.rows());
-
 		for(int i=0; i<generated.size(); i++)
-			assertEquals(generated.get(i), generatedFromDifferentImage.get(i), 0.01);
-		
-		//assertFalse(Arrays.equals(generated, generatedFromDifferentImage));
-	}
-
-	@Test
-	public void testGenerateFeatureVectorOfSubimage(){
-		List<Image> testDataset = TestsDataSetsHelper.loadDataSetClassImages(DataSet.calteckUniversity, Type.Test, "apple");
-		Image image = testDataset.get(0);
-		SubImage subImage = new SubImage(image, 5, 5, 100, 100);
-		SurfFeatureVectorGenerator generator = new SurfFeatureVectorGenerator();
-
-		generator.prepareGenerator(Arrays.asList(Arrays.asList(image)));
-		List<Double> featureVector;
-		featureVector = generator.generateFeatureVector(image);
-		assertEquals("feature vector rows != 64", 64, featureVector.size());
-
-		featureVector = generator.generateFeatureVector(subImage);
-		assertEquals("feature vector rows != 64", 64, featureVector.size());
+			assertNotEquals(generated.get(i), generatedFromDifferentImage.get(i), 0.0);
 	}
 }
--- src/test/java/grad/proj/utils/SubImageTest.java
+++ src/test/java/grad/proj/utils/SubImageTest.java
@@ -11,7 +11,7 @@ public class SubImageTest {
 	@Test
 	public void testGetPixelWithCorrectBounds() throws Exception{
 
-		Image originalImage = createTestImage();
+		Image originalImage = createRandomTestImage(10, 10);
 		
 		int startRow = 3, startCol = 5, width = 4, height = 4;
 		
@@ -26,7 +26,7 @@ public class SubImageTest {
 	
 	@Test(expected=IndexOutOfBoundsException.class)
 	public void testGetPixelWithOutOfBounds() throws Throwable{
-		Image originalImage = createTestImage();
+		Image originalImage = createRandomTestImage(10, 10);
 		
 		int startRow = 3, startCol = 5, width = 4, height = 4;
 		
@@ -37,19 +37,23 @@ public class SubImageTest {
 	@SuppressWarnings("unused")
 	@Test(expected=RuntimeException.class)
 	public void testConstructSubImageOutOfBoundsOfOriginalImage() throws Throwable{
-		Image originalImage = createTestImage();
+		Image originalImage = createRandomTestImage(10, 10);
 		
 		int startRow = 3, startCol = 5, width = 12, height = 4;
 		
 		Image subImage = new SubImage(originalImage, startCol, startRow, width, height);
 	}
+	
+	public static Image createRandomTestImage(int width, int height){
+		return createRandomTestImage(width, height, -1);
+	}
 
-	private Image createTestImage() {
+	public static Image createRandomTestImage(int width, int height, int color) {
 		Random random = new Random();
-		Image originalImage = new ImageImpl(10, 10);
+		Image originalImage = new ImageImpl(width, height);
 		for(int row=0; row<originalImage.getHeight(); row++){
 			for(int col=0; col<originalImage.getWidth(); col++){
-				originalImage.setPixelAt(row, col, random.nextInt());
+				originalImage.setPixelAt(row, col, color);
 			}
 		}
 		return originalImage;
